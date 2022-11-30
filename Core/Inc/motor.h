7143cbe0daf86b5d2b0d1aabae895c0420f9715b
hamzahannani <hamza.hannani@ensea.fr>
2022-11-30 11:43:32 +0100

    30/11/2022

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -13,6 +13,7 @@
 void motorPowerOn(void);
 void motorPowerOff(void);
 void motorSetSpeed(int speed);
+void HAL_GPIO_EXTI_CallBack(uint16_t GPIO_Pin);
 
 
 #endif /* INC_MOTOR_H_ */
--- Core/Src/motor.c
+++ Core/Src/motor.c
@@ -10,11 +10,13 @@
   */
 
 #include "motor.h"
+extern TIM_HandleTypeDef htim1;
 
 /**
   * @brief  Switch on the motor driver
   * @retval None
   */
+
 void motorPowerOn(void){
 	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin); // just for test, you can delete it
 }
@@ -33,5 +35,23 @@ void motorPowerOff(void){
   * @retval None
   */
 void motorSetSpeed(int speed){
-	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin); // just for test, you can delete it
+	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+	// just for test, you can delete it
+	__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,speed);
+	TIM1->CCR1 = speed;
+	__HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_2,1023-speed);
+
+}
+/*void init_motor(void){
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
+	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
+	motorSetSpeed(int speed)
+}*/
+
+void HAL_GPIO_EXTI_CallBack(uint16_t GPIO_Pin){
+	HAL_GPIO_WritePin(ISO_RESET_GPIO_Port, ISO_RESET_Pin, SET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(ISO_RESET_GPIO_Port, ISO_RESET_Pin, RESET);
 }
